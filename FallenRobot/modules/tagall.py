63048cd4f513ba627d5454d203a663dee6f3622f
sukunbots <110039556+sukunbots@users.noreply.github.com>
2022-08-06 14:34:07 +0530

    Update tagall.py

--- FallenRobot/modules/tagall.py
+++ FallenRobot/modules/tagall.py
@@ -103,5 +103,5 @@ __mod_name__ = "Tᴀɢ Aʟʟ​"
 __help__ = """
 ──「 Only for Admins 」──
 
-❍ /tagall or @all '(reply to message or add another message) To mention all members in your group, without exception.'
+✯ /tagall or @all '(reply to message or add another message) To mention all members in your group, without exception.'
 """
