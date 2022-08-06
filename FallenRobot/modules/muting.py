c260a315f35aa852032997e8f44ff6a2fe19f470
sukunbots <110039556+sukunbots@users.noreply.github.com>
2022-08-06 13:20:39 +0530

    Update muting.py

--- FallenRobot/modules/muting.py
+++ FallenRobot/modules/muting.py
@@ -241,9 +241,11 @@ def temp_mute(update: Update, context: CallbackContext) -> str:
 
 __help__ = """
 *Admins only:*
- ❍ /mute <userhandle>*:* silences a user. Can also be used as a reply, muting the replied to user.
- ❍ /tmute <userhandle> x(m/h/d)*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
- ❍ /unmute <userhandle>*:* unmutes a user. Can also be used as a reply, muting the replied to user.
+ ✯ /mute <userhandle>*:* silences a user. Can also be used as a reply, muting the replied to user.
+ ✯ /tmute <userhandle> x(m/h/d)*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
+ ✯ /unmute <userhandle>*:* unmutes a user. Can also be used as a reply, muting the replied to user.
+
+ᴘʟᴇᴀsᴇ sᴜᴘᴘᴏʀᴛ [ᴛᴇᴀᴍ sᴜᴋᴜɴ](https://t.me/sukunsupports)
 """
 
 MUTE_HANDLER = CommandHandler("mute", mute)
