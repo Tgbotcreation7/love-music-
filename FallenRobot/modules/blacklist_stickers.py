356f2291818531d22032826d13072a826d56fe6d
sukunbots <110039556+sukunbots@users.noreply.github.com>
2022-08-06 11:09:51 +0530

    Update blacklist_stickers.py

--- FallenRobot/modules/blacklist_stickers.py
+++ FallenRobot/modules/blacklist_stickers.py
@@ -505,6 +505,8 @@ Blacklist sticker is used to stop certain stickers. Whenever a sticker is sent, 
  ✯ /blstickermode <ban/tban/mute/tmute>*:* sets up a default action on what to do if users use blacklisted stickers
 Note:
  ✯ <sticker link> can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message
+ 
+ ᴘʟᴇᴀsᴇ sᴜᴘᴘᴏʀᴛ [ᴛᴇᴀᴍ sᴜᴋᴜɴ](https://t.me/sukunsupports)
 """
 
 __mod_name__ = "Bʟ-Sᴛɪᴄᴋ"
