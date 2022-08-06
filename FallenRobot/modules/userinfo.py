94fc9bbbe7b9aae0218049840098974e5d567269
sukunbots <110039556+sukunbots@users.noreply.github.com>
2022-08-06 14:48:31 +0530

    Update userinfo.py

--- FallenRobot/modules/userinfo.py
+++ FallenRobot/modules/userinfo.py
@@ -506,26 +506,28 @@ def __user_info__(user_id):
 
 __help__ = """
 *ID:*
- ❍ /id*:* get the current group id. If used by replying to a message, gets that user's id.
- ❍ /gifid*:* reply to a gif to me to tell you its file ID.
+ ✯ /id*:* get the current group id. If used by replying to a message, gets that user's id.
+ ✯ /gifid*:* reply to a gif to me to tell you its file ID.
 
 *Self added information:* 
- ❍ /setme <text>*:* will set your info
- ❍ /me*:* will get your or another user's info.
+ ✯ /setme <text>*:* will set your info
+ ✯ /me*:* will get your or another user's info.
 *Examples:* 💡
  ➩ /setme I am a wolf.
  ➩ /me @username(defaults to yours if no user specified)
 
 *Information others add on you:* 
- ❍ /bio*:* will get your or another user's bio. This cannot be set by yourself.
- ❍ /setbio <text>*:* while replying, will save another user's bio 
+ ✯ /bio*:* will get your or another user's bio. This cannot be set by yourself.
+ ✯ /setbio <text>*:* while replying, will save another user's bio 
 *Examples:* 💡
  ➩ /bio @username(defaults to yours if not specified).`
  ➩ /setbio This user is a wolf` (reply to the user)
 
 *Overall Information about you:*
- ❍ /info*:* get information about a user. 
- ❍ /myinfo*:* Shows info about the user who sent this command.
+ ✯ /info*:* get information about a user. 
+ ✯ /myinfo*:* Shows info about the user who sent this command.
+
+ᴘʟᴇᴀsᴇ sᴜᴘᴘᴏʀᴛ [ᴛᴇᴀᴍ sᴜᴋᴜɴ](https://t.me/sukunsupports)
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
